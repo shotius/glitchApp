1cf90788c004da02d6cd598bc9fd17ce757a0d27
Glitch (cooing-working-amazonsaurus) <none>
2020-11-07 13:48:21 +0000

    🛬👻 Checkpoint ./public/style.css:24662156/38 ./views/index.html:24662156/112 ./server.js:24662156/138

--- server.js
+++ server.js
@@ -24,6 +24,12 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/whoami", (req, res) => {
+  res.json({ipaddress:"not set",
+           language: "not set"})
+})
+
+
 
 
 // listen for requests :)
